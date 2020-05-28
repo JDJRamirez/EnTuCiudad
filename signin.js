3b54bb7cc65683c45cc988ec4b24f721070b332f
legend41-1 <32181393+legend41-1@users.noreply.github.com>
2020-05-28 17:39:31 -0500

    video perdon add

--- signin.js
+++ signin.js
@@ -14,3 +14,28 @@ function onSignIn(googleUser)
     var id_token = googleUser.getAuthResponse().id_token;
     console.log("ID Token: " + id_token);
   }
+
+  window.fbAsyncInit = function()
+  {
+    FB.init({
+      appId      : '{2493604780902683}',
+      cookie     : true,
+      xfbml      : true,
+      version    : '{v7.0}'
+    });    
+    FB.AppEvents.logPageView();       
+  };
+
+  (function(d, s, id)
+  {
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+   FB.getLoginStatus(function(response)
+   {
+       statusChangeCallback(response);
+   });
